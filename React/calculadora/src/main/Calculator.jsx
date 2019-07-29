e166ba812f861d86419b541756934dc589cb648e
0102-ilegra <mauriciosoto@rede.ulbra.br>
2019-07-29 15:56:40 -0300

    Create Buttons & Display

--- React/calculadora/src/main/Calculator.jsx
+++ React/calculadora/src/main/Calculator.jsx
@@ -1,29 +1,31 @@
 import React, {Component} from 'react'
 import './Calculator.css'
 import Button from '../components/Button'
+import Display from '../components/Dispĺay'
 
 export default class Calculator extends Component {
     render(){
         return(
             <div>
                 <div className="calculator">
-                <Button label="AC"/>
-                <Button label="/"/>
-                <Button label="7"/>
-                <Button label="8"/>
-                <Button label="9"/>
-                <Button label="x"/>
-                <Button label="4"/>
-                <Button label="5"/>
-                <Button label="6"/>
-                <Button label="-"/>
-                <Button label="1"/>
-                <Button label="2"/>
-                <Button label="3"/>
-                <Button label="+"/>
-                <Button label="0"/>
-                <Button label="."/>
-                <Button label="="/>
+                    <Display value={100}/>
+                    <Button label="AC"/>
+                    <Button label="/"/>
+                    <Button label="7"/>
+                    <Button label="8"/>
+                    <Button label="9"/>
+                    <Button label="x"/>
+                    <Button label="4"/>
+                    <Button label="5"/>
+                    <Button label="6"/>
+                    <Button label="-"/>
+                    <Button label="1"/>
+                    <Button label="2"/>
+                    <Button label="3"/>
+                    <Button label="+"/>
+                    <Button label="0"/>
+                    <Button label="."/>
+                    <Button label="="/>
             </div>
             </div>
            
