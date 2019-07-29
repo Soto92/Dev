a87d81d8eea2a98367b374e2b8d35dba337daae6
0102-ilegra <mauriciosoto@rede.ulbra.br>
2019-07-29 09:10:09 -0300

    add funcionalidades update e delete no projeto Challenge em React

--- Consumindo API da Marvel em JS/main.js
+++ Consumindo API da Marvel em JS/main.js
@@ -3,12 +3,11 @@ const btnHome = document.getElementById('home');
 let list = document.querySelector('#list');
 let date = new Date();
 let ts = Date.now(); //getTime XD82
-const publicKey = 'c2c80a3a6c10fa95806ee502681d6d4';
-const privateKey = '24438e70865a9787d82aee470efab0514dd0cdb';
+const publicKey = '8c2c80a3a6c10fa95806ee502681d6d4';
+const privateKey = '224438e70865a9787d82aee470efab0514dd0cdb';
 const hash = MD5(ts + privateKey + publicKey);
 
-
-btnHome.addEventListener('click', () => home());
+btnHome.addEventListener('click', home);
 
 function home() {
     console.log(list.childNodes);
@@ -31,7 +30,7 @@ function mostreDesc(hero, itemList) {
 
 btn.addEventListener('click', function () {
     home();
-    const URL = `https://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+    const URL = `https://gateway.marvel.com/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
     fetch(URL).
     then(function (response) {
         return response.json();
@@ -39,12 +38,10 @@ btn.addEventListener('click', function () {
     then(function (response) {
         console.log(response);
         response.data.results.forEach(function (hero) {
-
             let item = document.createElement('li');
             item.classList.add('stiloL');
             const photo = `${hero.thumbnail.path}/landscape_medium.${hero.thumbnail.extension}`;
-            item.innerHTML = '<img src="' + photo + '"/> <span>' + hero.name + '</span> ';
-
+            item.innerHTML = '<img src="' + photo + '"/> <span>' + hero.title + '</span> ';
             list.appendChild(item);
             item.addEventListener('click', () => mostreDesc(hero, item));
         })
