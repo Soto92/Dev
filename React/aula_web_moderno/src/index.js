7b8d62468289b5bf17f755c1513db4fbe2332141
0102-ilegra <mauriciosoto@rede.ulbra.br>
2019-07-29 10:47:10 -0300

    Maneiras de exportar metodos em react

--- React/aula_web_moderno/src/index.js
+++ React/aula_web_moderno/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Primeiro from './componentes/Primeiro'
+import BomDia from './componentes/Bom-Dia'
 
-ReactDOM.render(<Primeiro />, document.getElementById('root'));
+ReactDOM.render(<BomDia nome="Mauricio" />, document.getElementById('root'));
 
 
