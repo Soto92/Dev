598eba38d2c8547a95392687dd66f4055efa2a75
0102-ilegra <mauriciosoto@rede.ulbra.br>
2019-07-30 13:30:00 -0300

    Introduction Node

---
+++ Node/moduleB.js
@@ -0,0 +1,6 @@
+module.esports = {
+    bomDia: 'Bom dia',
+    boaNoite() {
+        return 'Boa noite'
+    }
+}
---
+++ Node/moduleClient.js
@@ -0,0 +1,7 @@
+const moduleA = require('./moduleA')
+const moduleB = require('./moduleB')
+
+console.log(moduleA.ola)
+console.log(moduleA.bemVindo)
+console.log(moduleA.ateLogo)
+console.log(moduleA)
